a54fa75adf394870054364c2c238c6f414b86d87
Toshik <toshik@toshik.com>
2017-02-21 23:11:54 +0300

    preliminary support for AVR Arduino usage

--- TickerScheduler.cpp
+++ TickerScheduler.cpp
@@ -1,4 +1,3 @@
-#include <Arduino.h>
 #include "TickerScheduler.h"
 
 void tickerFlagHandle(volatile bool * flag)
@@ -7,7 +6,7 @@ void tickerFlagHandle(volatile bool * flag)
         *flag = true;
 }
 
-TickerScheduler::TickerScheduler(uint size)
+TickerScheduler::TickerScheduler(uint8_t size)
 {
     this->items = new TickerSchedulerItem[size];
     this->size = size;
@@ -15,7 +14,7 @@ TickerScheduler::TickerScheduler(uint size)
 
 TickerScheduler::~TickerScheduler()
 {
-    for (uint i = 0; i < this->size; i++)
+    for (uint8_t i = 0; i < this->size; i++)
     {
         this->remove(i);
         yield();
@@ -38,7 +37,7 @@ void TickerScheduler::handleTicker(tscallback_t f, volatile bool * flag)
     }
 }
 
-bool TickerScheduler::add(uint i, uint32_t period, tscallback_t f, boolean shouldFireNow)
+bool TickerScheduler::add(uint8_t i, uint32_t period, tscallback_t f, boolean shouldFireNow)
 {
     if (i >= this->size || this->items[i].is_used)
         return false;
@@ -51,7 +50,7 @@ bool TickerScheduler::add(uint i, uint32_t period, tscallback_t f, boolean shoul
     return enable(i);
 }
 
-bool TickerScheduler::remove(uint i)
+bool TickerScheduler::remove(uint8_t i)
 {
     if (i >= this->size || !this->items[i].is_used)
         return false;
@@ -64,7 +63,7 @@ bool TickerScheduler::remove(uint i)
     return true;
 }
 
-bool TickerScheduler::changePeriod(uint i, uint32_t period)
+bool TickerScheduler::changePeriod(uint8_t i, uint32_t period)
 {
     if (i >= this->size || !this->items[i].is_used)
         return false;
@@ -74,7 +73,7 @@ bool TickerScheduler::changePeriod(uint i, uint32_t period)
     return true;
 }
 
-bool TickerScheduler::disable(uint i)
+bool TickerScheduler::disable(uint8_t i)
 {
     if (i >= this->size || !this->items[i].is_used)
         return false;
@@ -84,7 +83,7 @@ bool TickerScheduler::disable(uint i)
     return true;
 }
 
-bool TickerScheduler::enable(uint i)
+bool TickerScheduler::enable(uint8_t i)
 {
     if (i >= this->size || !this->items[i].is_used)
         return false;
@@ -96,29 +95,28 @@ bool TickerScheduler::enable(uint i)
 
 void TickerScheduler::disableAll()
 {
-    for (uint i = 0; i < this->size; i++)
-    {
-        if (this->items[i].is_used)
-            this->items[i].t.detach();
-    }
+    for (uint8_t i = 0; i < this->size; i++)
+        disable(i);
 }
 
 void TickerScheduler::enableAll()
 {
-    for (uint i = 0; i < this->size; i++)
-    {
-        if (this->items[i].is_used)
-            this->items[i].t.attach_ms(this->items[i].period, tickerFlagHandle, &this->items[i].flag);
-    }
+    for (uint8_t i = 0; i < this->size; i++)
+        enable(i);
 }
 
 void TickerScheduler::update()
 {
-    for (uint i = 0; i < this->size; i++)
+    for (uint8_t i = 0; i < this->size; i++)
     {
-        if (this->items[i].is_used)
-            handleTicker(this->items[i].cb, &this->items[i].flag);
+		if (this->items[i].is_used)
+		{
+			#ifdef ARDUINO_ARCH_AVR
+			this->items[i].t.Tick();
+			#endif
 
+			handleTicker(this->items[i].cb, &this->items[i].flag);
+		}
         yield();
     }
 }
